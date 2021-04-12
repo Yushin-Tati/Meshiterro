c9c6d98009a760ee0591a1f71d09543bc51db011
Yushin-Tati <y.tati8653@gmail.com>
2021-04-12 13:47:55 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'fad3c83e6237981e2ebe2ebcc2afa065a5af1431589307ce652de862f06408bf7f36a6aa9cf453354bfbce26d627bc9c675585a333b1ea677cceff04d02661ef'
